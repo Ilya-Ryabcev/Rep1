21e7255902400e905a6e54dbc08b17f9c413bc01
Илья <bull.81@list.ru>
2025-03-21 16:15:09 +0300

    Сохраняй copy1

---
+++ Templates.py
@@ -0,0 +1,9 @@
+# 1. Поиск максимального элемента
+my_list = [22,54,76,99,23,23]
+maximum_element = my_list[0]
+for i in my_list:
+    if i > maximum_element:
+        maximum_element = i
+
+print(maximum_element)
+
