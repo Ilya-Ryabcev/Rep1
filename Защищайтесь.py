0d990bf62bff221cc49cb5f4906f3131d7ecd389
Илья <bull.81@list.ru>
2025-03-26 18:30:06 +0300

    Сохраняй copy1

---
+++ Защищайтесь.py
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Car:
+#     def __init__(self, brand, model, year):
+#         self.brand = brand
+#         self._model = model
+#         self.__year =year
+
+
+# car = Car('1', '2', 2000)
+# print(car._model)
+# car._model = 10
+# print(car._model)
+
+# car = Car('1', '2', 2020)
+# print(car._Car__year)
+# car._Car__year = 3000
+# print(car._Car__year)
+
+    # def get_model(self):
+    #     return self._model
+    #
+    # def set_model(self, model):
+    #     self._model = model
+    #     return self._model
+#
+# car = Car('1', '2', '2000')
+# print(car.get_model())
+# print(car.set_model("100500"))
+
+    # def __print_breand(self):
+    #     print('brand')
+    #
+    # def public_breand(self):
+    #     self.__print_breand()
+#
+# car = Car('1', '2', 2020)
+# car.public_breand()
--- Полиморфизм.py
+++ Полиморфизм.py
@@ -1,32 +1,32 @@
-import math
-
-class Shape:
-    def area(self):
-        raise NotImplementedError("Subclasses should implement this method!")
-
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * (self.radius ** 2)
-
-class Rectangle(Shape):
-    def __init__(self, wigth, height):
-        self.wigth = wigth
-        self.height = height
-
-    def area(self):
-        return self.wigth * self.height
-
-shapes = [Circle(5), Rectangle(5,10), Circle(5)]
-areas = [x.area() for x in shapes]
-
+# import math
+#
+# class Shape:
+#     def area(self):
+#         raise NotImplementedError("Subclasses should implement this method!")
+#
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         self.radius = radius
+#
+#     def area(self):
+#         return math.pi * (self.radius ** 2)
+#
+# class Rectangle(Shape):
+#     def __init__(self, wigth, height):
+#         self.wigth = wigth
+#         self.height = height
+#
+#     def area(self):
+#         return self.wigth * self.height
+#
+# shapes = [Circle(5), Rectangle(5,10), Circle(5)]
+# areas = [x.area() for x in shapes]
+#
 # for x in shapes:
 #     x.area()
-
-
-for i in areas:
-    print(i)
+#
+#
+# for i in areas:
+#     print(i)
 
 
