a5ccddbca813cb5d0aae24d8133303bf89e15b63
Илья <bull.81@list.ru>
2025-03-26 17:45:13 +0300

    Сохраняй copy1

---
+++ Полиморфизм.py
@@ -0,0 +1,26 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses should implement this method!")
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+class Rectangle(Shape):
+    def __init__(self, wigth, height):
+        self.wigth = wigth
+        self.height = height
+
+    def area(self):
+        return self.wigth * self.height
+
+shapes = [Circle(5), Rectangle(5,10), Circle(5)]
+areas = [x.area() for x in shapes]
+
+for i in areas:
+    print(i)
