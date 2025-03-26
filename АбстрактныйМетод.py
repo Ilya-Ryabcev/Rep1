1bd3b3100862aa5b267c71c317868289718c5ef0
Илья <bull.81@list.ru>
2025-03-26 18:40:16 +0300

    Сохраняй copy1

---
+++ АбстрактныйМетод.py
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ from abc import ABC, abstractmethod
+# class Vehicle(ABC):
+#     def __init__(self, brand):
+#         self.brand = brand
+#
+#     @abstractmethod
+#     def method(self):
+#         return 'Я гоняю по Москве'
+#
+# class Car(Vehicle):
+#     def __init__(self, brand, speed):
+#         super().__init__(brand)
+#         self.speed = speed
+#
+#     def method(self):
+#         return super().method() + " на байке"
+#
+#     def drive(self):
+#         print(f'Еду на машине {self.brand} {self.speed} км в час')
+#
+# car = Car('bmv', 200)
+# car.drive()
+# print(car.method())
