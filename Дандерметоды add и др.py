2052c67a00d996d123eaa4871a4a5dec282610f5
Илья <bull.81@list.ru>
2025-03-30 17:31:08 +0300

    Сохраняй copy1

---
+++ Дандерметоды add и др.py
@@ -0,0 +1,22 @@
+# Сложение __add__
+# Вычитание __sub__
+# Умножение __mul__
+# Деление __truediv__
+
+
+# class Path:
+#     def __init__(self, value):
+#         self.value = value
+#
+#     def __add__(self, other):
+#         return Path(self.value - other.value)
+#
+#     def __str__(self):
+#         return str(self.value)
+#
+# a = Path(100)
+# b = Path(5)
+# c = Path(15)
+# print(a + b + c)
+
+
