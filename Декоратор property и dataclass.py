34fe718f031cffe00079af2105570c324d8418ce
Илья <bull.81@list.ru>
2025-04-02 22:17:05 +0300

    Сохраняй copy1

--- Декоратор property и dataclass.py
+++ Декоратор property и dataclass.py
@@ -2,6 +2,8 @@
 # Также декоратор Property создает сеттер, который устанавливает значение результата функции
 # и делитер, который удаляет значение
 
+# Декоратор dataclass используется, если нужно обращаться к полям не по индексам а по именам
+
 # class Variable:
 #     def __init__(self, name, age):
 #         self.name = name
@@ -37,32 +39,42 @@
 # namber.multiplayer = 20
 # print(namber.resalt) # 50
 
-class Namber:
-    def __init__(self, value, multiplayer):
-        self.value = value
-        self.multiplayer = multiplayer
+# class Namber:
+#     def __init__(self, value, multiplayer):
+#         self.value = value
+#         self.multiplayer = multiplayer
+#
+#     @property
+#     def resalt(self):
+#         return self.value * self.multiplayer
+#
+#     @resalt.setter
+#     def resalt(self, value):
+#         self.resal = value
+#
+#     @resalt.deleter
+#     def resalt(self, value):
+#         self.resal = value
+#
+# namber = Namber(5, 10)
+# print(namber.resalt)
+# namber.value = 100
+# namber.multiplayer = 20
+# print(namber.resalt) # 2000
+#
+# namber.resal = 23
+# print(namber.resal)
+#
+# del namber.value
+# print(namber.value) # ошибка
 
-    @property
-    def resalt(self):
-        return self.value * self.multiplayer
+from dataclasses import dataclass
 
-    @resalt.setter
-    def resalt(self, value):
-        self.resal = value
-
-    @resalt.deleter
-    def resalt(self, value):
-        self.resal = value
-
-namber = Namber(5, 10)
-print(namber.resalt)
-namber.value = 100
-namber.multiplayer = 20
-print(namber.resalt) # 2000
-
-namber.resal = 23
-print(namber.resal)
-
-del namber.value
-print(namber.value) # ошибка
+# @dataclass()
+# class Humen:
+#     name: str
+#     age: int
+#     phone: str
+# humen = Humen('Stone', 40, 555555)
+# print(humen)
 
